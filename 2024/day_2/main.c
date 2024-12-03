590dadb1c05407e65c2def21f3b581023f8e272e
Papp Adam <papp.adam@students.jedlik.eu>
2024-12-03 10:35:54 +0100

    asd

--- 2024/day_2/main.c
+++ 2024/day_2/main.c
@@ -5,39 +5,35 @@ bool correnct_sequence(int elem, int last_element, int inc)
     return ((elem - last_element) * inc > 0 && abs(last_element - elem) < 4 && last_element != elem);
 }
 
+bool check_line_exc_index(uint32_tVector *vec, size_t index)
+{
+    for (int i = 0; i < vec->len; i++)
+    {
+        if (i == index)
+        {
+            continue;
+        }
+    }
+}
+
 int main()
 {
     FileReader reader = new_reader("day_2/input");
 
     int safe_count = 0;
+
     while (
         get_next_line(&reader))
     {
+        uint32_tVector nums = new_uint32_t_vec(4);
         stringVector line = split_string_by(reader.line_content, ";");
-        int last_element = to_int(line.ptr[0]);
-        int inc = to_int(line.ptr[1]) - last_element;
-        uint8_t safe = 1;
-        for (int i = 1; i < line.len; i++)
+        for (int i = 0; i < line.len; i++)
         {
-            int elem = to_int(line.ptr[i]);
-            if (!correnct_sequence(elem, last_element, inc))
-            {
-                if ((i > 2 && !correnct_sequence(elem, to_int(line.ptr[i - 2]), inc)))
-                {
-                    safe = 0;
-                    break;
-                }
-            }
-
-            last_element = elem;
-        }
-        safe_count += safe;
-        for (int i = 1; i < line.len; i++)
-        {
-            free(line.ptr[i]);
+            push_uint32_t(&nums, to_int(line.ptr[i]));
         }
 
-        free(line.ptr);
+        free_string_vec(&line);
+        free_uint32_t_vec(&nums);
     }
 
     printf("%i\n", safe_count);
